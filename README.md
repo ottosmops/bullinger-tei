b121629d060def3cff2888a4103d8b9bc88ccf6a
bernsc <bernard.schroffenegger@uzh.ch>
2022-11-30 03:50:18 +0100

    v1.1

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# BULLINGER CORPUS (TEI)
+# BULLINGER CORPUS (TEI-XML)
 
 ### Contents
 
-- Letters (1 file/letter): data/\*.xml
-- DTD (doctype definition): *tei.dtd*
+- Letters: data/letters/\*.xml [13'154 files; ~120 MB]
+- Portraits: data/portraits/*.(jpg|png) [453 files, ~220 MB]
+- Persons: data/persons.xml [~ 1 MB]
+- Places: data/places.xml [~ 0.1 MB]
+- DTD: *tei.dtd*
+- Validation: scripts/validate.py
+
+#### Letters
+
+- IDs $>$ 10'013: HBBW, edited letters
+- IDs $<=$ 10'013: unedited transcriptions
+
+##### Sources
+
+IRG (unless otherwise noted)
 
 ---
 
--- scripts/validate.py
+++ scripts/validate.py
@@ -6,25 +6,29 @@ from lxml import etree
 
 
 PATH_DTD = "tei.dtd"
-PATH_LETTERS = "data/"
+PATH_LETTERS = "data/letters/"
 
 
 class Validation:
 
     @staticmethod
-    def validate(path_dtd, path_corpus) -> bool:
-        dtd, is_valid, n = etree.DTD(open(path_dtd)), True, 0
+    def validate(path_dtd: str, path_corpus: str) -> bool:
+        dtd, is_valid, issues, errors = etree.DTD(open(path_dtd)), True, 0, 0
         for f in sorted(os.listdir(path_corpus)):
             if f != ".DS_Store":
-                tree = etree.parse(open(os.path.join(path_corpus, f)))
-                valid = dtd.validate(tree)
-                if not valid:
-                    print(f, ":\n", dtd.error_log.filter_from_errors())
-                    is_valid, n = False, n + 1
-        print("Issues:", n)
+                try:
+                    if not dtd.validate(etree.parse(open(os.path.join(path_corpus, f)))):
+                        print(f, dtd.error_log.filter_from_errors())
+                        is_valid, issues = False, issues + 1
+                except:
+                    print("FATAL ERROR in", f)
+                    errors += 1
+        print("Errors:", errors)
+        print("Issues:", issues)
         return is_valid
 
 
 if __name__ == '__main__':
 
-    Validation.validate(PATH_DTD, PATH_LETTERS)
+    if not Validation.validate(PATH_DTD, PATH_LETTERS): print("Validation FAILED.")
+    else: print("Is valid.")
